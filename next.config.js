e215ed08e40edf4e61cfe3edd0264eb2353206e3
ayomide-olayode <115666126+ayomide-olayode@users.noreply.github.com>
2026-06-03 06:54:39 +0100

    chore: update customWorkerSrc path in next-pwa configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   skipWaiting: true,
-  customWorkerSrc: "worker/index.ts",
+  customWorkerSrc: "worker",
   disable: process.env.NODE_ENV === "development",
 });
 const nextConfig = {
